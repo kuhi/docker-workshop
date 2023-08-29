87c5f72fd6abc26ddc27c02a45f09d6a7de96001
Yauheni <yauheni.kachalouski@telekom.com>
2023-08-29 17:49:05 +0200

    rollback container name; add initdb.sql

--- initdb/initdb.sql
+++ initdb/initdb.sql
@@ -1,3 +1,12 @@
+CREATE SCHEMA IF NOT EXISTS bookstore;
+
+CREATE TABLE IF NOT EXISTS bookstore.book (
+    id serial primary key,
+    name text not null,
+    year int not null,
+    author text not null
+);
+
 INSERT INTO bookstore.book
     (name, author, year)
 VALUES
--- sql/book.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS bookstore.book (
-    id serial primary key,
-    name text not null,
-    year int not null,
-    author text not null
-);
